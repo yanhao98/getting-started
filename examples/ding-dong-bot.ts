b344326e826476ac2e8de32664aa42a39a5289f8
严浩 <me@yanhao.ren>
2023-07-02 16:30:44 +0800

    :t

--- examples/ding-dong-bot.ts
+++ examples/ding-dong-bot.ts
@@ -7,10 +7,51 @@
 // https://github.com/motdotla/dotenv/issues/89#issuecomment-587753552
 import "dotenv/config.js";
 
-import { Contact, Message, ScanStatus, WechatyBuilder, log } from "wechaty";
+import { type Contact, type Message, ScanStatus, WechatyBuilder, log } from "wechaty";
 
 import qrcodeTerminal from "qrcode-terminal";
-import { translate } from "./chat";
+import axios, { type AxiosRequestConfig } from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+// ================================================================================
+
+async function translate(text: string) {
+  let config: AxiosRequestConfig = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://pandora.yanhao.ren/api/conversation/talk",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: {
+      prompt: `将以下文本翻译为 英语：\n${text}`,
+      message_id: uuidv4(),
+      conversation_id: "64e7d92d-5493-4124-9051-dbbd3b49c44d",
+      parent_message_id: "3b5eceea-c5b7-4357-aee9-54c3b2d38788",
+      model: "text-davinci-002-render-sha",
+      timezone_offset_min: -480,
+    },
+  };
+
+  const response = await axios.request(config);
+  console.debug(
+    "================================================================================"
+  );
+  const dataLines = (response.data.split("\n\n") as string[]).filter(
+    (line) => line !== "" && line !== "data: [DONE]"
+  );
+  const dataStr = dataLines[dataLines.length - 1];
+  if (dataStr) {
+    const data = JSON.parse(dataStr.replace("data: ", ""));
+    const parts = data.message.content.parts as string[];
+    return parts;
+  } else {
+    throw new Error("No data");
+  }
+}
+
+// translate("你好");
+
 
 // log.level("silly");
 // log.level("silent");
